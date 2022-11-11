51a450bfc3cdf29b0f08102714cc34e6ff6a86dc
PineapplePatriot <vitkovska.pn@ucu.edu.ua>
2022-11-11 12:04:32 +0200

    Finishing touches + input

--- game3.py
+++ game3.py
@@ -1,9 +1,10 @@
-def player_input(letters):
+def player_input():
     '''
     '''
+    letters = input('Enter a letter please >>> ')
     if isinstance(letters, str):
         letters = letters.lower()
         if len(letters) == 1 and ord(letters) in range(97, 123):
             return letters
         else:
-            return False
+            return " "
