606529d4f71092d9a04b4de0d353e9d4c4bd26f3
Kruhlov <kruhlov.pn@ucu.edu.ua>
2022-11-11 11:45:08 +0200

    change by my team

--- Yfile.py
+++ Yfile.py
@@ -1,15 +1,4 @@
-import random
-
-def wordfill(guest):
-    ''' 
-    function fitch make from _*len
-    to guested word
-    '''
-    global wordfilled
-
-
-
-def game():
+def game(word):
     print('Loading word list from file...')
     #task of team_1
     # with ('words.txt') as fil:
@@ -20,16 +9,17 @@ def game():
     print('I am thinking of a word that is',len(word),'letters long.')
     tryies=8
     letters=[]# full alfavit of ungested 
-    global wordfilled
     while tryies>0:
         print('------------')
         print('You have',tryies,'guesses left.')
         print('Available letters:',letters)
         guest=input('Please guess a letter: ')
         #task of team_3
-        if guest.lower in letters and guest in word:
-            print('Good guess:',wordfill(guest))
+        if guest.lower() in letters and guest in word:
             letters.pop(guest)
+            for i in letters:
+                wordfilled=word.replace(i,'_')
+            print('Good guess:',wordfilled)
             if wordfilled==word:
                 print('------------\nCongratulations, you won!')
                 return True
@@ -38,3 +28,4 @@ def game():
             tryies-=1
     print('-----------\nSorry, you ran out of guesses. The word was else.')
     return False
+
