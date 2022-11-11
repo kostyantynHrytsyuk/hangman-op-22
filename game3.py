b422a014817930eda3150d79d07a3c42105a9a3f
annakachmarchyk <kachmarchyk.pn@ucu.edu.ua>
2022-11-11 21:00:14 +0200

    corrected mistakes

--- game3.py
+++ game3.py
@@ -3,9 +3,9 @@ def player_input():
     Function checks if input is letter and returns it. Otherwise, function returns empty space.
     '''
     letters = input()
-    if isinstance(letters, str):
-        letters = letters.lower()
-        if len(letters) == 1 and ord(letters) in range(97, 123):
-            return letters
-        else:
-            return ' '
+    letters = letters.lower()
+    if len(letters) == 1 and ord(letters) in range(97, 123):
+        return letters
+    else:
+        return ' '
+print(player_input())
