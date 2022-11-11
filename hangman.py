2c6a1a1e2c249b6a906030770b86896dcdba7fa9
Nastunya04 <dynia.pn@ucu.edu.ua>
2022-11-11 12:21:45 +0200

    implemented other function

--- hangman.py
+++ hangman.py
@@ -12,7 +12,17 @@ def read_from_file(file_name):
     ranword = random.choice(ranlist[0])
     return ranword, len(ranlist[0])
 
-#вставте код двох інших команд
+def player_input():
+    '''
+    '''
+    letters = input('Enter a letter please >>> ')
+    if isinstance(letters, str):
+        letters = letters.lower()
+        if len(letters) == 1 and ord(letters) in range(97, 123):
+            return letters
+        else:
+            return ' '
+
 def output():
     endgame = False
     guesses = 8
