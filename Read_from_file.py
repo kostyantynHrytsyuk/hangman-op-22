879ea58734e52dfc177db02380e0aedaa8520a81
shcherbana <116552632+shcherbana@users.noreply.github.com>
2022-11-11 11:32:06 +0200

    doc new

--- Read_from_file.py
+++ Read_from_file.py
@@ -2,8 +2,9 @@ import random
 
 def read_from_file(file_name):
     '''
-    (list) -> (str)
+    (list) -> (tuple)
     Creats new empty list. Reads the given file and writes each element into list. With random library chooses a word.
+    Returns tuple first element of which - word,second lenght of list
     '''
     ranlist = []
     with open(file_name, "r") as file:
