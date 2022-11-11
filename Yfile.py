436890ce8b7c1f6627f2df5649cff5deb4309ffa
Kruhlov <kruhlov.pn@ucu.edu.ua>
2022-11-11 11:26:50 +0200

    Broken, it a skech of full progect

--- Yfile.py
+++ Yfile.py
@@ -11,21 +11,22 @@ def wordfill(guest):
 
 def game():
     print('Loading word list from file...')
-    with ('words.txt') as fil:
-        word=fil.readlines(random(0))
-        amount=fil.lineamount
-    print(amount,'words loaded.')
+    #task of team_1
+    # with ('words.txt') as fil:
+    #     word=fil.readlines(random(0))
+    #     amount=fil.lineamount
+    #print(amount,'words loaded.')
     print('Welcome to the game, Hangman!')
     print('I am thinking of a word that is',len(word),'letters long.')
     tryies=8
-    letters=[]
+    letters=[]# full alfavit of ungested 
     global wordfilled
     while tryies>0:
         print('------------')
         print('You have',tryies,'guesses left.')
         print('Available letters:',letters)
         guest=input('Please guess a letter: ')
-        #try guest=
+        #task of team_3
         if guest.lower in letters and guest in word:
             print('Good guess:',wordfill(guest))
             letters.pop(guest)
