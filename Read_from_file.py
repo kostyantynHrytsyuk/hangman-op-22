8e2955be09e3201c633ac089bc4709a77bb6c451
shcherbana <116552632+shcherbana@users.noreply.github.com>
2022-11-11 16:35:46 +0200

    Redacted code

--- Read_from_file.py
+++ Read_from_file.py
@@ -7,11 +7,12 @@ def read_from_file(file_name):
     Returns tuple first element of which - word,second lenght of list
     '''
     ranlist = []
-    with open(file_name, "r") as file:
-        for i in file:
-            if isinstance(i , str):
-                ranlist.append(i.split(' '))
-                ranword = random.choice(ranlist[0])
-                return ranword, len(ranlist[0])
-            else:
-                return None
+    if isinstance(file_name , str):
+        with open(file_name, "r") as file:
+            for i in file:
+                if isinstance(i , str):
+                    ranlist.append(i.split(' '))
+                    ranword = random.choice(ranlist[0])
+                    return ranword, len(ranlist[0])
+    else:
+        return None , None
