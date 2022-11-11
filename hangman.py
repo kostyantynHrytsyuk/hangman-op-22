6c3d9600c7ba076f48516beb8a24830b1819607d
yuliavistak <vistak.pn@ucu.edu.ua>
2022-11-11 12:15:42 +0200

    Changes in main function

--- hangman.py
+++ hangman.py
@@ -1,10 +1,24 @@
+import random
+def read_from_file(file_name):
+    '''
+    (list) -> (tuple)
+    Creats new empty list. Reads the given file and writes each element into list. With random library chooses a word.
+    Returns tuple first element of which - word,second lenght of list
+    '''
+    ranlist = []
+    with open(file_name, "r") as file:
+        for i in file:
+            ranlist.append(i.split(' '))
+    ranword = random.choice(ranlist[0])
+    return ranword, len(ranlist[0])
+
 #вставте код двох інших команд
 def output():
     endgame = False
     guesses = 8
     letters = "abcdefghijklmnopqrstuvwxyz"
-    word = Read_from_file("words.txt")[0]
-    list_len = Read_from_file("words.txt")[1]
+    word = read_from_file("words.txt")[0]
+    list_len = read_from_file("words.txt")[1]
     guessed_word = ["_ " for i in word]
     print(f"""Loading word list from file...
 {list_len} words loaded.
