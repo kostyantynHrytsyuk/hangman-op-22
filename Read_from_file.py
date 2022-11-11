988aa1eb909c7e6269a19fe382bc7b09fed38dae
shcherbana <116552632+shcherbana@users.noreply.github.com>
2022-11-11 12:27:57 +0200

    New version

--- Read_from_file.py
+++ Read_from_file.py
@@ -2,13 +2,17 @@ import random
 
 def read_from_file(file_name):
     '''
-    (list) -> (tuple)
+    (str) -> (tuple)
     Creats new empty list. Reads the given file and writes each element into list. With random library chooses a word.
     Returns tuple first element of which - word,second lenght of list
     '''
     ranlist = []
     with open(file_name, "r") as file:
         for i in file:
-            ranlist.append(i.split(' '))
-    ranword = random.choice(ranlist[0])
-    return ranword, len(ranlist[0])
+            if isinstance(i , str):
+                ranlist.append(i.split(' '))
+                ranword = random.choice(ranlist[0])
+                return ranword, len(ranlist[0])
+            else:
+                return None
+print(read_from_file("words.txt"))
