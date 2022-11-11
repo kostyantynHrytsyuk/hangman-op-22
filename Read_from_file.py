a5abca3bf05a6441a3991527beb41bcd840a5ea8
NastiaPlaskonis <plaskonis.pn@ucu.edu.ua>
2022-11-11 11:17:50 +0200

    doctexts for reading file

--- Read_from_file.py
+++ Read_from_file.py
@@ -1,6 +1,10 @@
 import random
 
 def read_from_file(file_name):
+    '''
+    (list) -> (str)
+    Creats new empty list. Reads the given file and writes each element into list. With random library chooses a word.
+    '''
     ranlist = []
     with open(file_name, "r") as file:
         for i in file:
