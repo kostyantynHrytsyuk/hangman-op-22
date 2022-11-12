98a76a958fdb0a05763df01e49c7b75411ea341f
Nastunya04 <dynia.pn@ucu.edu.ua>
2022-11-12 13:20:28 +0200

    added coments

--- hangman.py
+++ hangman.py
@@ -45,14 +45,19 @@ I am thinking of a word that is {len(word)} letters long.""")
         print(f"""You have {guesses} guesses left.
 Available letters: {letters}""")
         player_inp = input("Please guess a letter: ")
+        #перевіряємо, чи ввід - англійська буква, застосовуючи 2 функцію
         if player_input(player_inp) == "":
             print((f"Oops! Enter only 1 English letter"))
+        #перевіряємо, чи користувач ще не вводив цю букву
         elif player_input(player_inp) not in letters and player_input(player_inp) != "": 
             print((f"Oops! You've already guessed that letter: {''.join(guessed_word)}"))
+        #перевіряємо, чи є ця буква у слові
+        #Випадок 1: букви немає у слові
         elif player_input(player_inp) not in word:
             print((f"Oops! That letter is not in my word: {''.join(guessed_word)}"))
             letters = letters.replace(player_inp, "")
             guesses -= 1
+        #Випадок 2: буква є у слові
         elif player_input(player_inp) in letters:
             letters = letters.replace(player_inp, "")
             for j in word:
@@ -61,9 +66,11 @@ Available letters: {letters}""")
                     try_word[i] = " "
                     guessed_word[i] = j
             print(f"Good guess:{''.join(guessed_word)}")
+        #Вивід при виграші
         if ''.join(guessed_word) == word:
             print("""------------\nCongratulations, you won!""")
             endgame = True
+        #Вивід при програші
         elif guesses <= 0:
             print(f"""-----------\nSorry, you ran out of guesses. The word was '{word}'.""")
             endgame = True
